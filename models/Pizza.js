a633abeefff6ceda8b4411cd0e070b07b1aa3e2f
Jillium <86424914+Jillium@users.noreply.github.com>
2022-01-10 13:14:15 -0700

    view and add replies

--- models/Pizza.js
+++ models/Pizza.js
@@ -38,7 +38,7 @@ const PizzaSchema = new Schema({
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function () {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the pizza model using the PizzaSchema
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
 // add comment
 // /api/comments/<pizzaID>
@@ -11,6 +11,11 @@ router
 // /api/comments/<pizzaId>/<commentId>
 router
 .route('/:pizzaId/:commentId')
+.put(addReply)
 .delete(removeComment);
+
+router
+.route('/:pizzaId/:commentId/:replyId')
+.delete(removeReply);
 
 module.exports = router;
